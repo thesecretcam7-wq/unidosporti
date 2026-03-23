1d16248948d46ad1994dd087707f0f73aa6e6c41
thesecretcam7-wq <thesecretcam7@gmail.com>
2026-03-23 12:35:10 +0100

    Add config files

---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}'],
+    theme: { extend: { fontFamily: { sans: ['Plus Jakarta Sans', 'system-ui', 'sans-serif'] } } },
+      plugins: [],
+      } */
